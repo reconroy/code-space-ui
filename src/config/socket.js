93015704f4bbfa7ed064a41218d2e39bb6cf2b75
reconroy <codewithroy22@gmail.com>
2024-10-30 10:54:28 +0530

    socket.js shifted

--- src/config/socket.js
+++ src/config/socket.js
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io(import.meta.env.VITE_SOCKET_URL , {
   auth: {
-    token: localStorage.getItem('jwt'), // Send token with WebSocket connection
+    token: localStorage.getItem('jwt'),
   },
 });
 
